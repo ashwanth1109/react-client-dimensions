af9c24788eda507e686490ae3be08b2d05368804
ashwanth1109 <ashwanth1109@gmail.com>
2021-10-03 18:46:18 +0530

    fix: undefined access issue

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState, useCallback } from 'react';
 
+const isBrowser = () => typeof window !== 'undefined';
+
 export const useClientDimensions = () => {
   const getClientDimensions = useCallback(
     () =>
       // isBrowser check is for systems like gatsby which do server side rendering during build
-      typeof window === undefined // isBrowser check
-        ? { vw: 0, vh: 0 }
-        : { vw: window?.innerWidth, vh: window?.innerHeight },
+      isBrowser() && { vw: window?.innerWidth, vh: window?.innerHeight }, // isBrowser check
     []
   );
   const [dimensions, setDimensions] = useState(getClientDimensions());
