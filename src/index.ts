0248064baf701ee34529c54b293513ac646da37e
ashwanth1109 <ashwanth1109@gmail.com>
2021-10-03 18:36:56 +0530

    fix: breaking tests

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import { useEffect, useState, useCallback } from 'react';
 
 export const useClientDimensions = () => {
-  const isBrowser = typeof window !== undefined
   const getClientDimensions = useCallback(
-    // isBrowser check is for systems like gatsby which do server side rendering during build
-    () => isBrowser && ({ vw: window.innerWidth, vh: window.innerHeight }),
+    () =>
+      // isBrowser check is for systems like gatsby which do server side rendering during build
+      typeof window === undefined // isBrowser check
+        ? { vw: 0, vh: 0 }
+        : { vw: window.innerWidth, vh: window.innerHeight },
     []
   );
   const [dimensions, setDimensions] = useState(getClientDimensions());
 
-  const handleResize = useCallback(() => setDimensions(getClientDimensions()), [
-    getClientDimensions,
-  ]);
+  const handleResize = useCallback(
+    () => setDimensions(getClientDimensions()),
+    [getClientDimensions]
+  );
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window?.addEventListener('resize', handleResize);
+    return () => window?.removeEventListener('resize', handleResize);
   }, [handleResize]);
 
   return dimensions;
